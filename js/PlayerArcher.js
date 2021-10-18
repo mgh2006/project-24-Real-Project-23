1407666fe5fa37fcf4a05866ad87f02895003c4f
mgh2006 <mhaddad06@gmail.com>
2021-10-18 19:20:18 -0400

    s

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -17,8 +17,16 @@ class PlayerArcher {
   display() {
     var pos = this.body.position;
     var angle = this.body.angle;
-    
-    //add the code to move arrow up and down
+
+    if (keyIsDown(DOWN_ARROW) && angle < -73 ) {
+      angle += 1;
+      Matter.Body.setAngle(this.body, angle);
+    }
+
+    if (keyIsDown(UP_ARROW) && angle > -103) {
+      angle -=1;
+      Matter.Body.setAngle(this.body, angle);
+    }
 
     push();
     translate(pos.x, pos.y);
@@ -28,3 +36,4 @@ class PlayerArcher {
     pop();
   }
 }
+
