e010fc6ed437a6f070f390b0edefc88d4b25442c
mgh2006 <mhaddad06@gmail.com>
2021-10-18 19:13:23 -0400

    ok

--- sketch.js
+++ sketch.js
@@ -3,60 +3,51 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world, backgroundImg;
-var canvas, angle, tower, ground, cannon;
-var cannonBall;
-var balls =[]
+var engine, world;
+var canvas;
+var player, playerBase;
+
+
+
 function preload() {
-  backgroundImg = loadImage("./assets/background.gif");
-  towerImage = loadImage("./assets/tower.png");
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
 }
 
 function setup() {
-  canvas = createCanvas(1200, 600);
+  canvas = createCanvas(windowWidth, windowHeight);
+
   engine = Engine.create();
   world = engine.world;
-  angleMode(DEGREES)
-  angle = 20
 
-  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
-  World.add(world, ground);
+  angleMode(DEGREES);
 
-  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
-  World.add(world, tower);
+  var options = {
+    isStatic: true
+  };
 
-  cannon = new Cannon(180, 110, 130, 100, angle);
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
 }
 
 function draw() {
-  background(189);
-  image(backgroundImg, 0, 0, width, height);
+  background(backgroundImg);
 
   Engine.update(engine);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
 
-  rect(ground.position.x, ground.position.y, width * 2, 1);
-  push();
-  imageMode(CENTER);
-  image(towerImage, tower.position.x, tower.position.y, 160, 310);
-  pop();
-
-  cannon.display();
-  for(var I = 0;I<balls.length;I++){
-    showcannonballs(balls[I],I)
-  }
-}
-function keyReleased() { if (keyCode === DOWN_ARROW) {
-  balls[balls.length-1].shoot()
- } }
-function showcannonballs(ball,I){
-  if(ball){
-    ball.display()
-  }
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
 }
 
-function keyPressed() { if (keyCode === DOWN_ARROW) { 
-  var cannonBall = new CannonBall(cannon.x, cannon.y);
-   cannonBall.trajectory = [];
-   Matter.Body.setAngle(cannonBall.body, cannon.angle);
-   balls.push(cannonBall); } 
-  }
+
+
+
